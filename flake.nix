5f04b212bba26eee01e97006ccf665ab09e506dd
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-01-28 18:23:42 -0600

    updates

--- flake.nix
+++ flake.nix
@@ -1,57 +1,65 @@
 {
-  inputs.nixpkgs.url = "nixpkgs";
-  inputs.unstable.url = "unstable";
+  description = "Boot.dev Git Course";
 
-  inputs.shell.url = "devshell";
-  inputs.parts.url = "flake-parts";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    parts.url = "flake-parts";
+    shell.url = "devshell";
+  };
 
   outputs = inputs @ { ... }:
     inputs.parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.shell.flakeModule ];
-
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-      perSystem = { pkgs, system, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            (final: _: {
-              unstable = import inputs.unstable {
-                inherit (final) system;
-              };
-            })
-          ];
-        };
+      imports = [
+        inputs.shell.flakeModule
+      ];
 
+      perSystem = { lib, pkgs, system, ... }: {
         devshells.default = {
-          devshell.packages = with pkgs; [
-            # Formatters
-            nixpkgs-fmt
-            taplo
+          imports = [
+            "${inputs.shell}/extra/language/c.nix"
+            "${inputs.shell}/extra/language/go.nix"
+            "${inputs.shell}/extra/language/rust.nix"
           ];
+
+          language = {
+            c.compiler = pkgs.clang;
+            rust.packageSet = pkgs.rustPackages;
+          };
 
           commands = with pkgs; [
             {
-              package = git;
-            }
-            {
-              package = treefmt;
+              package = "treefmt";
             }
             {
               package = pkgs.callPackage buildGoModule rec {
                 name = "bootdev";
                 pname = name;
-                version = "1.11.0";
-                vendorHash = "sha256-jhRoPXgfntDauInD+F7koCaJlX4XDj+jQSe/uEEYIMM=";
-                meta.description = "The official command line tool for Boot.dev. It allows you to submit lessons and do other such nonsense.";
+                version = "1.16.1";
+
                 src = fetchFromGitHub {
                   owner = "bootdotdev";
                   repo = "bootdev";
                   rev = "v${version}";
-                  sha256 = "sha256-ZQW8UBm1oeo04dvBKB2MLrwbkV1hlxNVNiispuKJLMc=";
+                  sha256 = "sha256-e6LUAcG0tCTfRWGkJ85jIfjcr4/1fIP61rPPUTDrkjg=";
                 };
+
+                vendorHash = "sha256-jhRoPXgfntDauInD+F7koCaJlX4XDj+jQSe/uEEYIMM=";
+
+                meta.description = "The official command line tool for Boot.dev. It allows you to submit lessons and do other such nonsense.";
               };
             }
+          ];
+
+          devshell.packages = with pkgs; [
+            nixpkgs-fmt
+            mdformat
+            black
+            taplo
+            (python3.withPackages (p: with p; [
+              pygame
+            ]))
           ];
         };
       };
